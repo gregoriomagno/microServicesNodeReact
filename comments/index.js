df5bf5f56d691b65031815bb9d47d4745fb12f76
Gregório Magno Neto <gregorio.neto@maida.health>
2023-06-10 10:00:07 -0300

    feat: adicionando microservice que controla os comentarios

---
+++ comments/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+  // teste/
+  const commentsId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentsId, content: content, status: "pending" });
+
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentsId,
+      content,
+      postId: req.params.id,
+      status: "pending",
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  console.log("Received Event", type);
+
+  if (type === "CommentModerated") {
+    const { postId, status, id,content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    });
+  }
+  res.send({});
+});
+app.listen(4003, () => {
+  console.log("listening on port 4003");
+});
